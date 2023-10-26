84a8e5f1d164c9f1295542969af474525b143d69
Thanh Luan <thanhluan.bkhn@gmail.com>
2023-10-26 12:10:04 +0700

    update 2023-10-26 12:10:04

---
+++ scripts/tools.ts
@@ -0,0 +1,70 @@
+import "../env-config";
+import { ethers } from "hardhat";
+
+import { configEnv } from "./@config";
+import { BigNumber, Contract, Signer } from "ethers";
+import { connectWallet, provider, sendMultipleNativeToken, sendMultipleToken } from "./@helpers/tools.helper";
+import { dateStrToSeconds, delayTime, parseAmountToken, stringDateToUTCDate } from "./@helpers/block-chain.helper";
+import { BatchTransferTool, BatchTransferTool__factory, ERC1155__factory, ERC20, ERC20__factory, ERC721, ERC721Enumerable, ERC721Enumerable__factory, ERC721Template, ERC721Template__factory, ERC721__factory, FeedVault, FeedVault__factory, MintNftFactory, MintNftFactoryV2, MintNftFactoryV2__factory, MintNftFactory__factory, StakeMultipleERC721, StakeMultipleERC721__factory, StakeNftAutoApy, StakeNftAutoApy__factory, StakeNftFactory, StakeNftFactory__factory, TokenERC721, TokenERC721__factory } from "../typechain";
+import { dir } from "console";
+import { writeFileSync } from "fs";
+import { join } from "path";
+import { isAddress } from "ethers/lib/utils";
+
+const { utils, constants, getSigners, getContractFactory } = ethers;
+const { formatEther } = utils;
+const { MaxUint256, Zero, AddressZero, HashZero } = constants;
+const {
+    NODE_ENV = "bscTestnet",
+} = process.env;
+
+
+const { TOKEN_ADDRESS, NETWORK_PROVIDER } = configEnv();
+const { URL_SCAN } = NETWORK_PROVIDER;
+
+
+async function main() {
+
+    const output: any = {};
+    const [deployer] = await getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+    const balance = await deployer.getBalance();
+    console.log("Account balance:", formatEther(balance));
+
+
+
+    const nftAddress = "";
+    const factoryAddress = "";
+    const factoryCt = new Contract(factoryAddress, MintNftFactoryV2__factory.abi, provider) as MintNftFactoryV2;
+
+    const mint = async ({ qty, owner }: { qty: number; owner: Signer }) => {
+        try {
+            const value = await factoryCt.getAmountOutByQty(nftAddress, qty);
+            const { transactionHash } = await ((await factoryCt.connect(owner).mintByQty(nftAddress, qty, AddressZero, {
+                value
+            })).wait())
+            return {
+                status: "DONE",
+                transactionHash,
+                owner: await owner.getAddress()
+            }
+        } catch (error) {
+            return {
+                status: "ERROR",
+                transactionHash: "",
+                owner: await owner.getAddress()
+            }
+        }
+    }
+
+
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
+// [1,2,3,4,5,6,7,8,9,10]
