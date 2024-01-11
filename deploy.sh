bb55f4c6301c313c90f71690bfd00032617992ff
Thanh Luan <thanhluan.bkhn@gmail.com>
2024-01-11 21:28:46 +0700

    update 2024-01-11 21:28:46

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-NODE_ENV=lineaMainnet yarn rex-fee-factory:deploy;
-NODE_ENV=lineaMainnet yarn rex-fee-factory:verify;
+NODE_ENV=ethMainnet yarn rex-fee-factory:deploy;
+NODE_ENV=ethMainnet yarn rex-fee-factory:verify;
 
 # NODE_ENV=lineaMainnet yarn free-mint-factory-v2:deploy;
 # NODE_ENV=lineaMainnet yarn free-mint-factory-v2:verify;
