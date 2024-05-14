97020413fcf40042721decd228af5328453f59dd
thanhluan.bkhn <thanhluan.bkhn@gmail.com>
2024-05-14 08:22:54 +0700

    update

--- scripts/social-vault/finalize.ts
+++ scripts/social-vault/finalize.ts
@@ -31,18 +31,28 @@ async function main() {
 
   console.log(`=====royaltyFee=====`);
 
-  const collectionAddress = "0x5938362da72af8b8e8ac39ed7ff184f075fb7734";
+  const collectionAddress = "0x4134fd50cd9f5cb276a326721fa8a0ab98680a77";
   const users = [
-    "0x22880225192c602b5caac4e5a202ab020f7b68e2".trim(),
-    "0x95e74c000e214ab8a63b78d10c009db9c456ac2a".trim(),
-    "0x3bbc0286571f155c02b480df2dbe9f133866b73a".trim(),
     "0x072BcaaD73D640912E63F04d71A98F18433aA306".trim(),
-    "0x6597eddfc40ccc8582115fdb8226b285df0c3256".trim(),
-    "0x1db75df0dd4c552b4d3642a20f3172d09d1ea10c".trim(),
-    "0x5ea8d52351f390c282bb18e48de317d63c0a3525".trim(),
-    "0x4658546a96dd2f78c385138f7493d30884c8ae05".trim(),
-    "0x389b4c9a4e6ddc5cd0805ced1f7f4fba78d4cfe5".trim(),
-    "0x9c188D52D238660D82134DD8dcec861B51971E30".trim(),
+    "0xbe5b1afae3d69e0b193e18ba75ea27e45af9d681".trim(),
+    "0xbc0ee07f70bfdcb17de9b2c75531611f1437180c".trim(),
+    "0xdf1448d858ef7a091202733e9d810ed0fc1b5d25".trim(),
+    "0xb68a2696c6f325f2ee87e06b3b8b20b9e6cfc722".trim(),
+    "0x45ee6aae03d2b261a5c980f16e41bc8f1f0e9551".trim(),
+    "0xaaa080ecac0bd5c978ff012f7c9386e87ad98336".trim(),
+    "0x9999c6ba5111604e7e3e4b08577686e28b29c39d".trim(),
+    "0x027e3df736a4eeba03395110327ffd2d2270f02b".trim(),
+    "0xc8be8bc0e266840005d3528e471c998de93479e3".trim(),
+    "0x07fd8d5418fecebc36f4168ad77a8ddff2e1520d".trim(),
+    "0x38746e4154844558ff71185bfcc4a20c23283d22".trim(),
+    "0x0d50bb5b6bcecc8a51898defbe2e9a89a82fea0a".trim(),
+    "0xa865a0338b404ce2def97f9604ff134f78b160b0".trim(),
+    "0x09b84784d76bed5ff07a3c38398c548182fa591c".trim(),
+    "0x46d14fcdf5c872e6b5aa2ba6c63309dea7d6c9cc".trim(),
+    "0x09f9b99715ce7ce972728c321eb9e66e687bfeb1".trim(),
+    "0x4822834930f7a7c500f45cb4b7937d679a502670".trim(),
+    "0x62ac9ed65f2c27c6b7e7654bf280e26630730fd5".trim(),
+    "0x3be97753da3d70678f5181fc7eacb6b70c72e22c".trim(),
   ];
   const { transactionHash } = await (
     await poolCt.connect(deployer).finalize(collectionAddress, users)
--- token.deploy.sh
+++ token.deploy.sh
@@ -1,7 +1,10 @@
-NODE_ENV=blastMainnet yarn social-vault:deploy;
-NODE_ENV=blastMainnet yarn social-vault:verify;
+# NODE_ENV=blastMainnet yarn social-vault:deploy;
+# NODE_ENV=blastMainnet yarn social-vault:verify;
 
 # NODE_ENV=blastMainnet yarn social-vault:create-campaign;
 
 # NODE_ENV=blastMainnet yarn social-vault:finalize;
 
+
+
+NODE_ENV=opMainnet yarn social-vault:finalize;
